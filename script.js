ddfdafa547f49bb2f559759082f81e7fa0caaeaf
Nithin2801 <nithingundemnv@gmail.com>
2025-12-23 16:28:16 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,3 @@
-<<<<<<< HEAD
-const menuBtn = document.querySelector('.menu-btn');
-const navLinks = document.querySelector('.nav-links');
-
-menuBtn.addEventListener('click', () => {
-  navLinks.classList.toggle('show');
-});
-=======
 const menuBtn = document.querySelector('.menu-btn');
 const navLinks = document.querySelector('.nav-links');
 
